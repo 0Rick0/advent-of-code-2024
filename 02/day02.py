4b9c7c897246c0025ce5783094dc6b65f3291200
Rick Rongen <rickrongen@outlook.com>
2024-12-02 17:32:59 +0100

    feat: optimized part 2 to minimize mutation

--- 02/day02.py
+++ 02/day02.py
@@ -5,25 +5,25 @@ def parse_input(filename: str) -> list[list[int]]:
     with open(filename) as f:
         return [list(map(int, line.split())) for line in f.readlines()]
 
-def report_is_safe(report: list[int]) -> bool:
+def report_is_safe(report: list[int]) -> tuple[int, bool]:
     op = operator.gt if report[0] >= report[1] else operator.lt
-    for a, b in zip(report, report[1:]):
+    for idx, (a, b) in enumerate(zip(report, report[1:])):
         if not op(a, b):
             # print(f"not safe {a} {op.__name__} {b}")
-            return False
+            return idx, False
         if abs(a - b) > 3:
             # print(f"not safe {a} +- {b} > 3")
-            return False
+            return idx, False
     # print(f"safe {report}")
-    return True
+    return -1, True
 
 def part1(filename: str) -> int:
-    return sum(1 for _ in filter(report_is_safe, parse_input(filename)))
+    return sum(1 for report in parse_input(filename) if report_is_safe(report)[1])
 
 
-def mutate_report(report: list[int]) -> list[list[int]]:
+def mutate_report(report: list[int], idx) -> list[list[int]]:
     new_reports = []
-    for i in range(len(report)):
+    for i in range(max(idx - 1, 0), min(idx + 1, len(report)) + 1):
         new_report = list(report)
         del new_report[i]
         new_reports.append(new_report)
@@ -31,28 +31,25 @@ def mutate_report(report: list[int]) -> list[list[int]]:
 
 
 def part2(filename: str) -> int:
-    # This solution is not really optimized
-    # But the dataset is small enough that it doesn't matter
-    # To optimize this, a similar operator check can be done as report_is_safe around the index of the first unsafe value
-    # That way only 3 values have to be considered for removal
     result = 0
     for report in parse_input(filename):
-        if report_is_safe(report):
+        idx, safe = report_is_safe(report)
+        if safe:
             result += 1
             continue
-        for mutated_report in mutate_report(report):
-            if report_is_safe(mutated_report):
+        for mutated_report in mutate_report(report, idx):
+            if report_is_safe(mutated_report)[1]:
                 result += 1
                 break
     return result
 
 def main():
-    assert report_is_safe([1, 2])
-    assert report_is_safe([2, 1])
-    assert not report_is_safe([2, 1, 2])
-    assert not report_is_safe([1, 10])
-    assert not report_is_safe([1, 1])
-    assert not report_is_safe([1, 2, 2])
+    assert report_is_safe([1, 2])[1]
+    assert report_is_safe([2, 1])[1]
+    assert not report_is_safe([2, 1, 2])[1]
+    assert not report_is_safe([1, 10])[1]
+    assert not report_is_safe([1, 1])[1]
+    assert not report_is_safe([1, 2, 2])[1]
     assert part1('sample.txt') == 2
     print(part1('input.txt'))
 
